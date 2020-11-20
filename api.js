96047a255b1a923c255bff7fe73a3e81e9314410
Cesar1299 <43218945+Cesar1299@users.noreply.github.com>
2020-11-20 09:48:39 -0400

    apicedula

--- api.js
+++ api.js
@@ -1,38 +1,16 @@
-var Usuario = require('./Usuario');
 const dboperations = require('./dboperations');
-
 var express = require('express');
 var bodyParser= require('body-parser');
 var cors = require('cors');
 var app = express();
 var router = express.Router();
 
-var PORT = process.env.PORT || 9090;
-app.listen(PORT);
-console.log('User API is runnning at ' + PORT);
+
 
 app.use(bodyParser.urlencoded({ extended: true}));
 app.use(bodyParser.json());
 app.use(cors());
 app.use('/api', router);
-
-
-dboperations.getUsuarios().then(result =>{
-    console.log(result);
-})
-
-router.use((request,response,next)=>{
-    console.log('middleware');
-    next();
- })
- 
-router.route('/usuarios').get((request,response)=>{
- 
-    dboperations.getUsuarios().then(result => {
-        response.json(result[0]);
-     })
- 
- })
  
  router.route('/usuarios/:cedula').get((request,response)=>{
  
@@ -41,3 +19,8 @@ router.route('/usuarios').get((request,response)=>{
      })
  
  })
+
+ //Puerto 
+var PORT = process.env.PORT || 1240;
+app.listen(PORT);
+console.log('User API is runnning at ' + PORT);
